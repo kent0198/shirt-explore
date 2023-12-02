4682a49a51c2bd39036ff26db24394b86d8668b6
Kem <tuetran25112002@gmail.com>
2023-12-02 22:20:12 +0700

    slideAnimation motion

--- shirt-3/src/pages/Home.tsx
+++ shirt-3/src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import {useSnapshot} from 'valtio'
 import state from '@store/index'
+import { logothree } from '@asset/index'
 
 const Home = () => {
-    
+    const snap=useSnapshot(state)
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        {
+          snap.intro && 
+          (<motion.section className=''>
+              <motion.header>
+                <img src={logothree} alt="logo" className='w-8 h-8 object-contain' />
+              </motion.header>
+          </motion.section>
+        )}
+    </AnimatePresence>
   )
 }
 
