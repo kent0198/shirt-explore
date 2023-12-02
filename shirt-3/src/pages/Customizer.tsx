c45f8ad0eaa5c4258fe3e139f986d8c91a2650f5
Kem <tuetran25112002@gmail.com>
2023-12-03 03:14:43 +0700

    ++up

--- shirt-3/src/pages/Customizer.tsx
+++ shirt-3/src/pages/Customizer.tsx
@@ -4,11 +4,15 @@ import { useSnapshot } from "valtio"
 import config from "@config/config"
 import state from "@store/index"
 import { download } from "@asset/index"
+import { downloadCanvasToImage, reader } from "@config/helpers"
+import { EditorTabs,FilterTabs, DecalTypes } from "@config/contants"
+import { fadeAnimation,slideAnimation } from "@motion/motion"
+
 const Customizer = () => {
   return (
-    <div>
+    <AnimatePresence>
 
-    </div>
+    </AnimatePresence>
   )
 }
 
