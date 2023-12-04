3fbdee3fedb612815dce81f8498c08d69641d7f4
Kem <tuetran25112002@gmail.com>
2023-12-05 02:25:18 +0700

    ++upcamera not fix

--- shirt-3/src/canvas/CameraRight.tsx
+++ shirt-3/src/canvas/CameraRight.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import state from '@store/index';
+import { easing } from 'maath';
+import React, {ReactNode} from 'react'
+import {useRef} from 'react'
+import { useSnapshot } from 'valtio';
+import * as THREE from 'three';
+interface prop{
+  children  : ReactNode;
+}
 
-const CameraRight = () => {
+const CameraRight:React.FC<prop>= ({children }) => {
+  const group = useRef<THREE.Group>(new THREE.Group());
+  const snap=useSnapshot(state)
+ /*  easing.dampE() */
   return (
-    <>CameraRight</>
+    <group ref={group}>
+    {children }
+    </group>
   )
 }
 
--- shirt-3/src/canvas/CanvasLayout.tsx
+++ shirt-3/src/canvas/CanvasLayout.tsx
@@ -1,5 +1,5 @@
-import {Canvas} from '@react-three/fiber'
-import {Environment,Center} from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
 import Shirt from './Shirt'
 import BackDrop from './BackDrop'
 import CameraRight from './CameraRight'
@@ -7,11 +7,13 @@ import CameraRight from './CameraRight'
 const CanvasLayout = () => {
   return (
     <Canvas>
-        <ambientLight intensity={0.5}/>
-        <Environment preset='city'/>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      <CameraRight>
         <Center>
-           <Shirt/>
+          <Shirt />
         </Center>
+      </CameraRight>
     </Canvas>
   )
 }
