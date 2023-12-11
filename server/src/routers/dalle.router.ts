88dd989b61b644d77dc8a139a4cf7e2fec66814d
Kem <tuetran25112002@gmail.com>
2023-12-11 22:06:04 +0700

    ++upcode generate image use openAI

--- server/src/routers/dalle.router.ts
+++ server/src/routers/dalle.router.ts
@@ -1,21 +1,40 @@
-import express, { Express, Request, Response } from 'express';
-import * as dotenv from 'dotenv'
-import { ClientOptions, OpenAI} from 'openai';
-import Configuration from "openai"
+import express, { Express, Request, Response } from "express";
+import * as dotenv from "dotenv";
+import { ClientOptions, OpenAI } from "openai";
+import Configuration from "openai";
 
-dotenv.config()
-const router=express.Router();
+dotenv.config();
+const router = express.Router();
 
-const config= new OpenAI({
-    apiKey:process.env.OPENAI_API_KEY,
+const config = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 }) as unknown as ClientOptions;
 
-const openai= new OpenAI(config)
+const openai = new OpenAI(config);
 
+router.route("/").get((req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTERS" });
+});
 
-router.route('/').get((req:Request,res:Response)=>{
-    res.status(200).json({message:'Hello from DALL.E ROUTERS'})
-})
-export default router
+interface Image {
+  data: { b64_json: string }[];
+}
 
-
+router.route("/").post(async (req: Request, res: Response) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const images: Image[] = response.data as Image[];
+    const firstImageData = images[0]?.data[0]?.b64_json;
+    res.status(200).json({ photo: firstImageData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+export default router;
