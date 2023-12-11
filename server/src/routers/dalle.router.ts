ab71d2cff737bf0caf90d86e17419481a75836ce
Kem <tuetran25112002@gmail.com>
2023-12-11 21:46:06 +0700

    ++fix OpenAI

--- server/src/routers/dalle.router.ts
+++ server/src/routers/dalle.router.ts
@@ -1,14 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv'
-import { OpenAI} from 'openai';
+import { ClientOptions, OpenAI} from 'openai';
 import Configuration from "openai"
 
 dotenv.config()
 const router=express.Router();
 
-const config= new Configuration({
-    apiKey:process.env.OPENAL_API_KEY
-})
+const config= new OpenAI({
+    apiKey:process.env.OPENAI_API_KEY,
+}) as unknown as ClientOptions;
+
 const openai= new OpenAI(config)
 
 
@@ -16,3 +17,5 @@ router.route('/').get((req:Request,res:Response)=>{
     res.status(200).json({message:'Hello from DALL.E ROUTERS'})
 })
 export default router
+
+
